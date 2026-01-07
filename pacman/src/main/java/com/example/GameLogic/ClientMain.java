3e033af170067ec47eb121dcf09c60879f1f29dd
christianwienelundquist <s235091@dtu.dk>
2026-01-07 12:07:34 +0100

    JavaFX rocks

--- pacman/src/main/java/com/example/GameLogic/ClientMain.java
+++ pacman/src/main/java/com/example/GameLogic/ClientMain.java
@@ -1,7 +1,8 @@
 package com.example.GameLogic;
 
 import com.example.model.*;
-
+import javafx.application.Application;
+import com.example.UI.UI;
 public class ClientMain {
  
     //private jSpaceStack rawAction;
@@ -11,4 +12,8 @@ public class ClientMain {
     private GameState gameState;
     private GameState saveState;
     private ClientGameController gameController;
+    
+    public static void main(String[] args) {
+        Application.launch(UI.class, args);
+    }
 }
