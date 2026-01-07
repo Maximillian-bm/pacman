b018b0b519066c2307568afcc55713d8e8199341
Gustav Aggeboe <gustavaggeboe@hotmail.com>
2026-01-07 13:40:21 +0100

    Add TileType and update GameState variables

--- pacman/src/main/java/com/example/model/GameState.java
+++ pacman/src/main/java/com/example/model/GameState.java
@@ -3,7 +3,11 @@ package com.example.model;
 import java.util.*;
 
 public class GameState {
- 
+    private int clock;
+
     private List<Player> players;
     private List<Ghost> ghosts;
+    private TileType[][] tiles;
+
+    private Player winner;
 }
