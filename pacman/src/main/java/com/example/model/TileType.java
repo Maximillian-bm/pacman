0c250166cc25610b416767b5cad8af54446e4d7c
Gustav Aggeboe <gustavaggeboe@hotmail.com>
2026-01-07 14:35:03 +0100

    Refactor FRUIT to the different fruit types in TileType

--- pacman/src/main/java/com/example/model/TileType.java
+++ pacman/src/main/java/com/example/model/TileType.java
@@ -3,7 +3,16 @@ package com.example.model;
 public enum TileType {
     EMPTY,
     WALL,
+    POWERUP,
     PELLET, // Gives points when eating
-    FRUIT,  // Gives a lot of points when eating
-    POWERUP
+
+    // FRUIT - Gives a lot of points when eating
+    CHERRY,     // 100
+    STRAWBERRY, // 300
+    ORANGE,     // 500
+    APPLE,      // 700
+    MELON,      // 1000
+    GALAXIAN,   // 2000
+    BELL,       // 3000
+    KEY         // 5000
 }
