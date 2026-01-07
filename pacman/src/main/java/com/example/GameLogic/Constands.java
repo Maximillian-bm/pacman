ba1851e524ee6d2ed8b82c862e982b0bfc58d1d1
maximillian <maxwork.bm@gmail.com>
2026-01-07 13:46:23 +0100

    jspace

---
+++ pacman/src/main/java/com/example/GameLogic/Constands.java
@@ -0,0 +1,8 @@
+package com.example.GameLogic;
+
+import org.jspace.SpaceRepository;
+
+public class Constands {
+    public final static String SPACE_URI = "tcp://127.0.0.1:9001/?rep";
+    public final static SpaceRepository rep = new SpaceRepository();
+}
