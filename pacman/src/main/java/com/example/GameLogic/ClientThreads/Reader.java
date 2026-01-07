e53ead7918bbf16d902ef39fedb130877d96b5d5
maximillian <maxwork.bm@gmail.com>
2026-01-07 15:54:25 +0100

    reader

--- pacman/src/main/java/com/example/GameLogic/ClientThreads/Reader.java
+++ pacman/src/main/java/com/example/GameLogic/ClientThreads/Reader.java
@@ -1,11 +1,30 @@
 package com.example.GameLogic.ClientThreads;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.jspace.ActualField;
+import org.jspace.FormalField;
+import org.jspace.RemoteSpace;
+import org.jspace.Space;
+
+import com.example.GameLogic.ClientMain;
+import com.example.GameLogic.Constands;
+
 public class Reader implements Runnable {
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        try {
+            Space remoteActions = new RemoteSpace(Constands.REMOTE_URI);
+            Object[] t = remoteActions.query(new ActualField(ClientMain.nrOfActions), new FormalField(Integer.class), new FormalField(Integer.class), new FormalField(Integer.class));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
  
 }
--- pacman/src/main/java/com/example/GameLogic/Constands.java
+++ pacman/src/main/java/com/example/GameLogic/Constands.java
@@ -4,5 +4,6 @@ import org.jspace.SpaceRepository;
 
 public class Constands {
     public final static String SPACE_URI = "tcp://127.0.0.1:9001/?rep";
+    public final static String REMOTE_URI = "tcp://XXX.XXX.X.XXX/?rep";
     public final static SpaceRepository rep = new SpaceRepository();
 }
