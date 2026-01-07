f11cf8ad957a68c1abede39015bc533b6ecc780a
Gustav Aggeboe <gustavaggeboe@hotmail.com>
2026-01-07 15:23:48 +0100

    Enhance `Player` behavior with pellet points and improved ghost-eating logic. Add `POINTS_PER_PELLET` constant.

--- pacman/src/main/java/com/example/model/Constants.java
+++ pacman/src/main/java/com/example/model/Constants.java
@@ -5,4 +5,7 @@ import org.jspace.SpaceRepository;
 public class Constants {
     public final static String SPACE_URI = "tcp://127.0.0.1:9001/?rep";
     public final static SpaceRepository rep = new SpaceRepository();
+
+    // Game constants
+    public final static int POINTS_PER_PELLET = 10;
 }
--- pacman/src/main/java/com/example/model/Player.java
+++ pacman/src/main/java/com/example/model/Player.java
@@ -2,19 +2,28 @@ package com.example.model;
 
 public class Player extends Entity {
     private int id;
-    public Position position;
-    public Direction direction;
+    private Position position;
+    private Direction direction;
 
     private int points;
     private int hearts;
-    private int ghostsEaten;
+
+    /*
+     * From: "https://pacman.fandom.com/wiki/Power_Pellet"
+     * If consecutive ghosts are eaten during the same Energizer effect, they will give out 400, 800 and 1,600 point bonuses for each of the consecutive ghosts in order.
+     */
+    private int ghostsEatenThisEnergizer;
 
     public void updateDir(Direction direction) {
         this.direction = direction;
     }
 
+    public void eatPellet() {
+        points += Constants.POINTS_PER_PELLET;
+    }
+
     public void eatGhost() {
-        points += switch (ghostsEaten) {
+        points += switch (ghostsEatenThisEnergizer) {
             case 0 -> 200;
             case 1 -> 400;
             case 2 -> 800;
@@ -22,6 +31,6 @@ public class Player extends Entity {
             default -> 3200;
         };
 
-        ghostsEaten++;
+        ghostsEatenThisEnergizer++;
     }
 }
