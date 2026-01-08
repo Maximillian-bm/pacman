2812769cceb171dcc2a6f46c878ec69e41d4c105
maximillian <maxwork.bm@gmail.com>
2026-01-08 11:07:29 +0100

    server logic

--- pacman/src/main/java/com/example/model/Constants.java
+++ pacman/src/main/java/com/example/model/Constants.java
@@ -6,8 +6,9 @@ import org.jspace.SpaceRepository;
 
 public class Constants {
     public final static ArrayList<Action> cleanActions = new ArrayList<>();
-    public final static String REMOTE_URI = "tcp://XXX.XXX.X.XXX/cleanAction?rep";
-    public final static SpaceRepository rep = new SpaceRepository();
+    public final static String REMOTE_URI_CLEAN = "tcp://XXX.XXX.X.XXX/cleanAction?rep";
+    public final static String REMOTE_URI_RAW = "tcp://XXX.XXX.X.XXX/rawAction?rep";
+    public final static String GATE_URI = "tcp://XXX.XXX.X.XXX/?rep";
 
     // Game constants
     public final static int PLAYER_HEALTH = 1;
