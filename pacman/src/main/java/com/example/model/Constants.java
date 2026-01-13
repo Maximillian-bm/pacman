f9e976623a66ab7ee217c2ddfe164460941f6c24
maximillian <maxwork.bm@gmail.com>
2026-01-13 13:43:15 +0100

    made lobby ttl a constants

--- pacman/src/main/java/com/example/model/Constants.java
+++ pacman/src/main/java/com/example/model/Constants.java
@@ -7,7 +7,11 @@ import org.jspace.SpaceRepository;
 import static com.example.model.Maps.map1;
 
 public class Constants {
+    //Varibles
     public static int clock = 0;
+
+    //Constants
+    public final static long LOBBY_TTL = 300000;
     public final static int NR_OF_LOBBYS_CAP = 100;
     public final static ActionList cleanActions = new ActionList();
     public final static String REMOTE_PUBLIC_URI = "tcp://pacman.maximillian.info:50000/?keep";
