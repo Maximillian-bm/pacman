beaed0e35fe509052ccf49a7be028abf441ff65d
maximillian <maxwork.bm@gmail.com>
2026-01-14 13:50:41 +0100

    pom

--- pacman/src/main/java/com/example/GUI/SoundEngine.java
+++ pacman/src/main/java/com/example/GUI/SoundEngine.java
@@ -1,15 +1,14 @@
 package com.example.GUI;
 
-import java.io.File;
-
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 public class SoundEngine {
 
-    private String musicFile = "example.mp3";
-    private Media sound = new Media(new File(musicFile).toURI().toString());
-    private MediaPlayer mediaPlayer = new MediaPlayer(sound);
+    String soundFile = "path/to/your/sound.wav";
+    Media media = new Media(new File(soundFile).toURI().toString());
+    MediaPlayer mediaPlayer = new MediaPlayer(media);
 
     public void startMusic() {
         mediaPlayer.play();
