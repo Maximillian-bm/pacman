cd2f3c043e98301e5abe20f77ec53d5d002aaf41
Toby <60399231+TobyNatooor@users.noreply.github.com>
2026-01-15 11:09:21 +0100

    renamed all music files

--- pacman/src/main/java/com/example/model/Sound.java
+++ pacman/src/main/java/com/example/model/Sound.java
@@ -1,22 +1,22 @@
 package com.example.model;
 
 public enum Sound {
-    CREDIT("sounds/01. Credit Sound.mp3"),
-    START_MUSIC("sounds/02. Start Music.mp3"),
-    EAT_DOT("sounds/03. PAC-MAN - Eating The Pac-dots.mp3"),
-    TURNING_CORNER("sounds/04. PAC-MAN - Turning The Corner While Eating The Pac-dots.mp3"),
-    EXTEND("sounds/05. Extend Sound.mp3"),
-    GHOST_NORMAL("sounds/06. Ghost - Normal Move.mp3"),
-    GHOST_SPURT_1("sounds/07. Ghost - Spurt Move #1.mp3"),
-    GHOST_SPURT_2("sounds/08. Ghost - Spurt Move #2.mp3"),
-    GHOST_SPURT_3("sounds/09. Ghost - Spurt Move #3.mp3"),
-    GHOST_SPURT_4("sounds/10. Ghost - Spurt Move #4.mp3"),
-    EAT_FRUIT("sounds/11. PAC-MAN - Eating The Fruit.mp3"),
-    GHOST_BLUE("sounds/12. Ghost - Turn to Blue.mp3"),
-    EAT_GHOST("sounds/13. PAC-MAN - Eating The Ghost.mp3"),
-    GHOST_HOME("sounds/14. Ghost - Return to Home.mp3"),
-    FAIL("sounds/15. Fail.mp3"),
-    COFFEE_BREAK("sounds/16. Coffee Break Music.mp3");
+    CREDIT("sounds/01_credit_sound.mp3"),
+    START_MUSIC("sounds/02_start_music.mp3"),
+    EAT_DOT("sounds/03_pacman_eating_The_pacdots.mp3"),
+    TURNING_CORNER("sounds/04_pacman_turning_the_corner_while_eating_the_pacdots.mp3"),
+    EXTEND("sounds/05_extend_sound.mp3"),
+    GHOST_NORMAL("sounds/06_ghost_normal_move.mp3"),
+    GHOST_SPURT_1("sounds/07_ghost_spurt_move1.mp3"),
+    GHOST_SPURT_2("sounds/08_ghost_spurt_move2.mp3"),
+    GHOST_SPURT_3("sounds/09_ghost_spurt_move3.mp3"),
+    GHOST_SPURT_4("sounds/10_ghost_spurt_move4.mp3"),
+    EAT_FRUIT("sounds/11_pacman_eating_the_fruit.mp3"),
+    GHOST_BLUE("sounds/12_ghost_turn_to_blue.mp3"),
+    EAT_GHOST("sounds/13_pacman_eating_the_ghost.mp3"),
+    GHOST_HOME("sounds/14_ghost_return_to_home.mp3"),
+    FAIL("sounds/15_fail.mp3"),
+    COFFEE_BREAK("sounds/16_coffee_break_music.mp3");
 
     private final String path;
 
