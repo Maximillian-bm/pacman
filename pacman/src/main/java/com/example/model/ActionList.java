464939cfccdf68d1cc0079731ba75fcfec30803e
maximillian <maxwork.bm@gmail.com>
2026-01-20 10:27:10 +0100

    removed variable action offset

--- pacman/src/main/java/com/example/model/ActionList.java
+++ pacman/src/main/java/com/example/model/ActionList.java
@@ -32,10 +32,7 @@ public class ActionList {
 
     public boolean missedAction(int clock){
         if(actionsList.size() > nrOfActionsCalled && actionsList.get(nrOfActionsCalled).clock() < clock){
-            if(actionsList.get(nrOfActionsCalled).getPlayerId() == playerID){
-                Constants.actionOffset++;
-                System.out.println("you missed your own action, action offset is now set to "+Constants.actionOffset+"game ticks");
-            }else{
+            if(actionsList.get(nrOfActionsCalled).getPlayerId() != playerID){
                 Constants.timeOffset = Constants.timeOffset + (500000000/Constants.TARGET_FPS);
                 double temp = (double) Constants.timeOffset /((double) 1000000000 /Constants.TARGET_FPS);
                 System.out.println("you missed another players action, your clock offset is now set to "+temp+" game ticks");
